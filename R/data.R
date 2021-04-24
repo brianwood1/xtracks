fef7a8ecf06ef61d5f16c6c68885a834429483df
Brian Wood <brianwood1@gmail.com>
2021-04-24 09:55:11 -0700

    documentation updates and fixing d1 and d2

--- R/data.R
+++ R/data.R
@@ -1,37 +1,7 @@
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
-#' @format A data frame with 8260 rows and 6 variables:
-#' \describe{
-#'   \item{lat}{latitude, in decimal degrees, WGS 84}
-#'   \item{lon}{longitude, in decimal degrees, WGS 84}
-#'   \item{elevation_m}{elevation in meters}
-#'   \item{in_camp}{binary flag to indicate if the individual is in the camp / residential area, or out of the camp / residential area}
-#'   \item{unix_time}{the GPS timestamp, encoded as the number of seconds since January 1st, 1970 UTC timezone}
-#'   \item{distance_from_camp_m}{the distance of the individual from the centroid of the camp / residential area}
-#' }
-"exd_1"
-
-#' Individual GPS trajectory dataset 1 
-#'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
-#' @format A data frame with 8005 rows and 6 variables:
-#' \describe{
-#'   \item{lat}{latitude, in decimal degrees, WGS 84}
-#'   \item{lon}{longitude, in decimal degrees, WGS 84}
-#'   \item{elevation_m}{elevation in meters}
-#'   \item{in_camp}{binary flag to indicate if the individual is in the camp / residential area, or out of the camp / residential area}
-#'   \item{unix_time}{the GPS timestamp, encoded as the number of seconds since January 1st, 1970 UTC timezone}
-#'   \item{distance_from_camp_m}{the distance of the individual from the centroid of the camp / residential area}
-#' }
-"exd_2"
-
-#' Individual GPS trajectory dataset 1 
-#'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with 8260 rows and 6 variables:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -43,10 +13,10 @@
 #' }
 "dx"
 
-#' Individual GPS trajectory dataset 
+#' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -60,8 +30,8 @@
 
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #'  @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -75,8 +45,8 @@
 
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -90,8 +60,8 @@
 
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -103,10 +73,10 @@
 #' }
 "d4"
 
-#' Individual GPS trajectory dataset 
+#' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -120,8 +90,8 @@
 
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -135,8 +105,8 @@
 
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -150,8 +120,8 @@
 
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
@@ -165,8 +135,8 @@
 
 #' Individual GPS trajectory dataset
 #'
-#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.  
-#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped. 
+#' Demonstration GPS data formatted in manner to facilitate the construction of an xtrack object and demonstration of package xtracks features.
+#' For privacy concerns, the raw spatial data have been shifted such that this track will appear to be in the center of Lake Eyasi when mapped.
 #' @format A data frame with one row per trackpoint and 6 variables per trackpoint:
 #' \describe{
 #'   \item{lat}{latitude, in decimal degrees, WGS 84}
